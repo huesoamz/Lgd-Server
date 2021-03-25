773c77ae6e0159951e53caedec195ce027787f28
huesoamz <huesoamz@gmail.com>
2021-03-25 12:42:20 -0300

    Create server-signals-reloads-examples.sql
    BY @MrLego#9584 from Discord Development

---
+++ Databases/server-signals-reloads-examples.sql
@@ -0,0 +1,15 @@
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 4, 1, 0, 0 );  /*Monster Reload BY @MrLego#9584*/
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 1, 1, 0, 0 );  /*npc Reload BY @MrLego#9584*/
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 1, 1, 0, 0 );    /*Shop reload BY @MrLego#9584 */
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 2, 1, 0, 0 ); /*Quest reload BY @MrLego#9584 */
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 3, 1, 0, 0 );  /*Event_Manager Reload BY @MrLego#9584 */
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 5, 1, 0, 0 ); /*Config Reload BY @MrLego#9584 */
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 6, 1, 0, 0 ); /*Item Reload BY @MrLego#9584 */
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 7, 1, 0, 0 );  /*Character Reload BY @MrLego#9584 */
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 8, 1, 0, 0 ); /*Skill Reload BY @MrLego#9584 */
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 9, 1, 0, 0 ); /*Teleport Reload BY @MrLego#9584 */
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 10, 1, 0, 0 ); /*Cash_Shop Reload BY @MrLego#9584 */
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 11, 1, 0, 0 ); /*Mix Reload BY @MrLego#9584 */
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 12, 1, 0, 0 ); /*World Reload BY @MrLego#9584 */
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 13, 1, 0, 0 ); /*Active Reload BY @MrLego#9584 */
+INSERT INTO database_characters.server_signal ( `server`,  `signal`,  `data_1`,  `data_2`,  `data_3`) VALUES ( 0, 14, 1, 0, 0 ); /*Pentagram Reload BY @MrLego#9584 */
